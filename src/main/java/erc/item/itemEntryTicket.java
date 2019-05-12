cc83eb0bf0d362714f48f946f13e08f50d2745d4
darthvader45 <alienkiller45@gmail.com>
2019-05-12 16:54:01 -0400

    Create itemEntryTicket.java
    Add entry ticket item.

---
+++ src/main/java/erc/item/itemEntryTicket.java
@@ -0,0 +1,13 @@
+package erc._mc.item
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import erc._core.ERC_Core;
+
+public class itemEntryTicket extends Item
+{
+    public class itemEntryTicket()
+    {
+      setCreativeTab(ERC_Core.ERC_Tab)
+    }
+}
